a4817adb15b099181dabd31eeb8367e98017369b
sanket064 <sanket064@gmail.com>
2019-07-07 16:48:55 -0700

    starter_files

---
+++ fail.php
@@ -0,0 +1,19 @@
+<?php
+$page_title = "Welcome to my fail page";
+include("libs/header.php");
+?>
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12 fail-background-color">
+<h1 class="fail-h1">Term 1</h1>
+</div>
+<div class="col-sm-12 fail-background-color">
+    <h2 class="fail-h2">Computer Basics</h2>
+    <h3 class="fail-h3">42&#37;<span class="ml-5">F</span></h3>
+    </div>
+</div>
+
+
+
+
+<?php include ("libs/footer.php")?>
