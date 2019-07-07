227ec20135da76ab75bb3309063155975127ef92
AhmedM0 <48606846+AhmedM0@users.noreply.github.com>
2019-07-07 16:49:36 -0700

    Ahmeds Html&Css
    Fixed the sass files, did the header, made a footer and the pending assignments page. :)

---
+++ pending.php
@@ -0,0 +1,20 @@
+<?php
+$page_title = "Pending";
+include("libs/header.php");
+?>
+<h1 class="pending_header">Pending Assignments</h1>
+<section id="pending_section">
+<div class="container">
+<div class="row">
+<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">   
+<h1 class="course_header">Computer Basics</h1>
+<h1 class="credit_header">Credits 1.0s</h1>
+<h1 class="assignment_header">Login Form</h1>
+<h3 class="description_header">Description</h3>
+<p class="course_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, iste porro quis incidunt error nostrum cum enim, corporis quae?</p>
+<p class="course_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, iste porro quis incidunt error nostrum cum praesentium magni aperiam tenetur vitae suscipit ullam, veritatis fugiat inventore totam enim, corporis quae?</p>
+<p class="course_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, iste porro quis incidunt error nostrum cum praesentium magni aperiam tenetur vitae suscipit ullam, veritatis fugiat inventore totam enim, corporis quae?</p>
+</section>
+
+
+<?php include ("libs/footer.php")?>
