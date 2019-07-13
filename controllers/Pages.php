50eaf74c66caf954597555a76dbbfaf1f7345707
Vikrant Rajan <vikrantrajan93@gmail.com>
2019-07-13 15:43:56 -0700

    Hey I cleaned the code a bit

--- controllers/Pages.php
+++ controllers/Pages.php
@@ -7,17 +7,11 @@ Class Pages extends Controller {
 
 	public function main()
 	{
-		// later output into the template.php
-		// $this->arrCats = Categories::getAll();	
 		$this->nav = $this->loadView("mainnav");
 		$this->mainbody .= $this->loadView("hero");
 
-		// get products from db
-		// $this->snippetTitle = Dictionary::get("featuredTitle","en");
-		// $this->arrFeatured = Products::getFeatured();
-		// $this->mainbody .= $this->loadView("productgrid");
 		$this->mainbody .= $this->loadView("userLogin");	
-		// $this->mainbody .= $this->loadView("userRegistration");
+		$this->mainbody .= $this->loadView("userRegistration");
 		include("views/template.php");
 	}
 
@@ -26,31 +20,13 @@ Class Pages extends Controller {
 	{
 		$this->nav = $this->loadView("mainnav");
 		$this->mainbody .= $this->loadView("hero_dashboard");
-		
 		include("views/template.php");
-	}
-
-	public function productsInCat()
-	{
-		// output data into template.php
-		// $this->arrCat = Category::get($_GET["cat"]); // load my cat object
-		// $this->arrCats = Categories::getAll();	
-		// $this->nav = $this->loadView("mainnav");
-
-		// $this->arrProducts = Products::getInCat($_GET["cat"]);
-		// $this->mainbody .= $this->loadView("productgridall");
-		// $this->arrComments = Comments::createComment();
-			
-		// include("views/template.php");
 	}
 
 	public function registerUser() {
 		
-		$this->arrCats = Categories::getAll();
 		$this->nav = $this->loadView("mainnav");
-		
-		$this->mainbody .= $this->loadView("register");
-			
+		$this->mainbody .= $this->loadView("register");	
 		include("views/template.php");
 	}
 
